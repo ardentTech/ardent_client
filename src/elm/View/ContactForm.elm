b05381ad2cd08765f216e9901672e7c47655a146
Jonathan D. Baker <jonathan@ardent.tech>
2018-08-03 16:11:56 -0600

    disable submit btn for now

--- src/elm/View/ContactForm.elm
+++ src/elm/View/ContactForm.elm
@@ -4,7 +4,7 @@ import Bulma.Form exposing (..)
 import Bulma.Elements exposing (..)
 import Bulma.Modifiers exposing (Color(Primary))
 import Html exposing (Html, div, span, text)
-import Html.Attributes exposing (placeholder)
+import Html.Attributes exposing (disabled, placeholder)
 
 import Message exposing (Msg(..))
 import Model exposing (Model)
@@ -24,6 +24,6 @@ contactFormView model =
       controlTextArea controlTextAreaModifiers [] [ placeholder "Message" ] []
     ],
     field [] [
-      controlButton { buttonModifiers | color = Primary } [] [] [ span [] [ text "Submit" ]]
+      controlButton { buttonModifiers | color = Primary, disabled = True } [] [] [ span [] [ text "Submit" ]]
     ]
   ]
